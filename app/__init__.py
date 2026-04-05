d02ae0001f8a4832383512220ccc0d1fce72901d
voanhkiet <voanhkiet261192@gmail.com>
2026-04-05 15:25:35 +0700

    fix proxy for file upload

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 db = SQLAlchemy()
 jwt = JWTManager()
@@ -11,7 +12,9 @@ bcrypt = Bcrypt()
 def create_app():
   app = Flask(__name__)
   app.config.from_object("config.Config")
-
+   # 🔥 FIX PROXY
+  app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+  
   CORS(
     app,
     resources={r"/api/*": {"origins": "*"}},
@@ -19,6 +22,7 @@ def create_app():
     allow_headers=["Content-Type", "Authorization"],
     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 )
+  
   db.init_app(app)
   jwt.init_app(app)
   bcrypt.init_app(app)
