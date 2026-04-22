8b2dfad7c8382ba737b6352958a7d5680ef4d006
voanhkiet <voanhkiet261192@gmail.com>
2026-04-22 14:11:08 +0700

    add role to user

--- app/models/user.py
+++ app/models/user.py
@@ -7,9 +7,13 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(200))
 
+    # NEW FIELD
+    role = db.Column(db.String(20), default="user")
+
     # New
     avatar_url = db.Column(db.String(255))
     bio = db.Column(db.Text)
+
 
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
